d69734d5d1c2de6b9b32dbeac2299e5e4515360c
dandingol03 <1291248710@qq.com>
2018-06-09 01:30:30 +0800

    add tab in MainActivity

--- app/src/main/java/com/outsource/danding/qingdaoic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/outsource/danding/qingdaoic/ui/activity/MainActivity.kt
@@ -3,30 +3,74 @@ package com.outsource.danding.qingdaoic.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import com.bumptech.glide.Glide
 import com.outsource.danding.qingdaoic.R;
 import com.outsource.danding.qingdaoic.base.BaseActivity
+import com.outsource.danding.qingdaoic.base.BaseTabActivity
+import com.outsource.danding.qingdaoic.ui.fragment.HomeFragment
 import com.outsource.danding.qingdaoic.util.GlideImageLoader
 import com.outsource.danding.qingdaoic.util.UIUtils
 import com.outsource.danding.qingdaoic.util.Utils
 import com.youth.banner.loader.ImageLoader
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_title_bar.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseTabActivity() {
+
+
+    override fun getFragmentClasses()= arrayOf(HomeFragment::class.java,HomeFragment::class.java,HomeFragment::class.java)
+
+    override fun getStrings(): Array<String> = arrayOf("申请","审核","我的")
+
+    override fun getIcons(): IntArray = intArrayOf(R.drawable.apply,R.drawable.audit,R.drawable.me)
 
     private var images= mutableListOf(R.drawable.banner,R.drawable.banner,R.drawable.banner)
 
+    override fun getTextColor(): Int = R.color.color_tab_text
+
+    override fun firstPosition(): Int = 0
+
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
         initListener()
     }
 
-    private fun initView() {
-        //开始轮播
-        banner.setImages(images).setImageLoader(GlideImageLoader()).start()
+    override fun isTabCanClick(position: Int): Boolean =
+            when (position) {
+
+                0 ->{
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+
+    override fun onTabSelected(position: Int) {
+        super.onTabSelected(position)
+        iv_start.visibility = View.GONE
+        iv_end.visibility = View.GONE
+        tv_end.visibility = View.GONE
+        toolbar.visibility = View.VISIBLE
+        when (position) {
+            0 -> {
+                title = getString(R.string.tab_home)
+                toolbar.visibility = View.GONE
+            }
+            else -> {
+
+            }
+        }
     }
+
+
+
 
 
     private fun initListener() {
---
+++ app/src/main/java/com/outsource/danding/qingdaoic/ui/fragment/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.outsource.danding.qingdaoic.ui.fragment
+
+import android.os.Bundle
+import com.outsource.danding.qingdaoic.R
+import com.outsource.danding.qingdaoic.base.BaseFragment
+import com.outsource.danding.qingdaoic.util.GlideImageLoader
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class HomeFragment:BaseFragment() {
+
+    private var images= mutableListOf(R.drawable.banner,R.drawable.banner,R.drawable.banner)
+
+    override fun setLayoutId(): Int = R.layout.fragment_home
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    private fun initView(){
+        banner.setImages(images).setImageLoader(GlideImageLoader()).start()
+    }
+
+    private fun initListener(){
+
+    }
+
+
+}
